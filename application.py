f1b40769347c13fda3cfb38cc61035c293156a48
Evan Falkenstine <falk225@gmail.com>
2018-12-03 16:26:54 -0500

    add relevant response for testing routes

--- application.py
+++ application.py
@@ -2,41 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/ItemCatalog')
 def viewAll():
-    return 'main page'
+    return 'Viewing all categories and items'
 
 @app.route('/ItemCatalog/Categories/Add', methods=['GET','POST'])
 def addCategory():
-    return 'new'
+    return 'Adding a new category'
 
 @app.route('/ItemCatalog/Categories/<int:categoryID>/Edit', methods=['GET','PUT'])
 def editCategory(categoryID):
-    return None
+    return 'Editing a category {}'.format(categoryID)
 
 @app.route('/ItemCatalog/Categories/<int:categoryID>/Delete', methods=['GET','DELETE'])
 def deleteCategory(categoryID):
-    return None
+    return 'Deleting a category {}'.format(categoryID)
 
 @app.route('/ItemCatalog/Categories/<int:categoryID>/Items/Add', methods=['GET','POST'])
 def addItem(categoryID):
     pass
-    return None
+    return 'Adding a new item to category {}'.format(categoryID)
 
 @app.route('/ItemCatalog/Items/<int:itemID>/Edit', methods=['GET','PUT'])
 def editItem(itemID):
     pass
-    return None
+    return 'Editing item {}'.format(itemID)
 
 @app.route('/ItemCatalog/Items/<int:itemID>/Delete', methods=['GET','DELETE'])
 def deleteItem(itemID):
     pass
-    return None
+    return 'Deleting item {}'.format(itemID)
 
 @app.route('/ItemCatalog/api')
 def jsonifyAll():
     pass
-    return None
+    return 'json of all categories and items'
     
 
 if(__name__ == '__main__'):
