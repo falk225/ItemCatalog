eca4fbfc7c4cfb9d1776031624e580f4261b759f
Evan Falkenstine <falk225@gmail.com>
2018-12-04 17:51:19 -0500

    fill in returns using render_template or redirect(url_for

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, User, Category, Item
@@ -12,68 +12,79 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/ItemCatalog')
-def viewAll():
-    return 'Viewing all categories and items'
+def viewCategories():
+    categories = session.query(Category).all()
+    return render_template('categoryView.html', categories=categories)
 
 @app.route('/ItemCatalog/Categories/Add', methods=['GET','POST'])
 def addCategory():
     if request.method == 'GET':
         #return add category form
-        pass
+        return render_template('categoryAdd.html')
     elif request.method == 'POST':
         #add new category
         pass
-    return 'Adding a new category'
+        return redirect(url_for('viewCategories'))
 
 @app.route('/ItemCatalog/Categories/<int:categoryID>/Edit', methods=['GET','PUT'])
 def editCategory(categoryID):
+    category = session.query(Category).filter_by(id=categoryID).one()
     if request.method == 'GET':
         #return edit category form
-        pass
+        return render_template('categoryEdit.html', category=category)
     elif request.method == 'PUT':
         #update category with user input
         pass
-    return 'Editing a category {}'.format(categoryID)
+        return redirect(url_for('viewCategories'))
 
 @app.route('/ItemCatalog/Categories/<int:categoryID>/Delete', methods=['GET','DELETE'])
 def deleteCategory(categoryID):
+    category = session.query(Category).filter_by(id=categoryID).one()
     if request.method == 'GET':
         #return confirm delete form
-        pass
+        return render_template('categoryDelete.html', category=category)
     elif request.method == 'POST':
         #delete category from databaes
         pass
-    return 'Deleting a category {}'.format(categoryID)
+        return redirect(url_for('viewCategories'))
+
+@app.route('/ItemCatalog/Categories/<int:categoryID>/Items/View')
+def viewItem(categoryID):
+    items = session.query(Item).filter_by(category_id).all()
+    return render_template('itemView.html', items=items)
 
 @app.route('/ItemCatalog/Categories/<int:categoryID>/Items/Add', methods=['GET','POST'])
 def addItem(categoryID):
     if request.method == 'GET':
         #return new item form
-        pass
+        return render_template('')
     elif request.method == 'POST':
         #add new item
         pass
-    return 'Adding a new item to category {}'.format(categoryID)
+        return redirect(url_for('viewItem', categoryID=categoryID))
 
 @app.route('/ItemCatalog/Items/<int:itemID>/Edit', methods=['GET','PUT'])
 def editItem(itemID):
+    item = session.query(Item).filter_by(id=itemID).one()
     if request.method == 'GET':
         #return edit item form
-        pass
+        return render_template('itemEdit.html', item=item)
     elif request.method == 'PUT':
         #make changes to item from user input
         pass
-    return 'Editing item {}'.format(itemID)
+        return redirect(url_for('viewItem', categoryID=item.category_id))
 
 @app.route('/ItemCatalog/Items/<int:itemID>/Delete', methods=['GET','DELETE'])
 def deleteItem(itemID):
+    item = session.query(Item).filter_by(id=itemID).one()
     if request.method == 'GET':
         #return confirm deletion page
-        pass
+        return render_template('itemDelete.html', id=itemID)
     elif request.method == 'DELETE':
+        category_id = item.category_id
         #remove item from database
         pass
-    return 'Deleting item {}'.format(itemID)
+        return redirect(url_for('viewItem', categoryID=category_id))
 
 @app.route('/ItemCatalog/api')
 def jsonifyAll():
