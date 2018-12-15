f8d8b782111b371cf4364d54b6f67821492fbe31
Evan Falkenstine <falk225@gmail.com>
2018-12-15 09:46:49 -0500

    require login for all add, edit, or delete routes

--- application.py
+++ application.py
@@ -1,24 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base, User, Category, Item
+import json
+import os
+import random
+import string
 from functools import wraps
-from flask import session as login_session
-
-from pkg_resources import resource_filename
 
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import httplib2
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
-
 import requests
-import json
+from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
+from flask import session as login_session
+from oauth2client.client import FlowExchangeError, flow_from_clientsecrets
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-import random, string
+from models import Base, Category, Item, User
 
-import os 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__)
@@ -29,17 +26,19 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if login_session['user'] is None:
+        if login_session.get('gplus_id') is None:
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
 
 @app.route('/login')
 def login():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
     # Use the client_secret.json file to identify the application requesting
     # authorization. The client ID (from that file) and access scopes are required.
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
@@ -68,20 +67,14 @@ def oauth2callback():
     if error:
         flash("Error Logging In: {}".format(error))
         return redirect(url_for('viewCategories'))
-    
+
     # Validate state token
     if request.args.get('state') != login_session['state']:
         flash("Error Logging In: {}".format("Invalid State"))
         return redirect(url_for('viewCategories'))
-    
+
     # Obtain authorization code
     code = request.args.get('code')
-
-    # credentials = client.credentials_from_clientsecrets_and_code(
-    #     filename='client_secret.json',
-    #     scope=['profile', 'email', 'openid'],
-    #     code=auth_code,
-    #     redirect_uri=url_for('oauth2callback', _external=True))
 
     try:
         # Upgrade the authorization code into a credentials object
@@ -89,8 +82,8 @@ def oauth2callback():
         oauth_flow.redirect_uri = url_for('oauth2callback', _external=True)
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
-        raise
-        flash("Error Logging In: {}".format("Failed to upgrade the authorization code."))
+        flash("Error Logging In: {}".format(
+            "Failed to upgrade the authorization code."))
         return redirect(url_for('viewCategories'))
 
     # Check that the access token is valid.
@@ -106,19 +99,23 @@ def oauth2callback():
     # Verify that the access token is used for the intended user.
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        flash("Error Logging In: {}".format("Token's user ID doesn't match given user ID."))
+        flash("Error Logging In: {}".format(
+            "Token's user ID doesn't match given user ID."))
         return redirect(url_for('viewCategories'))
 
     # Verify that the access token is valid for this app.
-    CLIENT_ID = json.loads(open('client_secret.json', 'r').read())['web']['client_id']
+    CLIENT_ID = json.loads(open('client_secret.json', 'r').read())[
+        'web']['client_id']
     if result['issued_to'] != CLIENT_ID:
-        flash("Error Logging In: {}".format("Token's client ID does not match app's."))
+        flash("Error Logging In: {}".format(
+            "Token's client ID does not match app's."))
         return redirect(url_for('viewCategories'))
 
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        flash("Error Logging In: {}".format('Current user is already connected.'))
+        flash("Error Logging In: {}".format(
+            'Current user is already connected.'))
         return redirect(url_for('viewCategories'))
 
     # Store the access token in the session for later use.
@@ -141,22 +138,25 @@ def oauth2callback():
         user_id = createUser(login_session)
         flash('New user {} added'.format(login_session['username']))
     login_session['user_id'] = user_id
-    
+
     flash("you are now logged in as {}".format(login_session['username']))
     return redirect(url_for('viewCategories'))
 
+
 def createUser(login_session):
     newUser = User(name=login_session['username'],
-        email=login_session['email'],
-        google_id=login_session['gplus_id'])
+                   email=login_session['email'],
+                   google_id=login_session['gplus_id'])
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
 
+
 def getUserInfo(user_id):
     user = session.query(User).filter_by(id=user_id).one()
     return user
+
 
 def getUserID(email):
     try:
@@ -164,6 +164,7 @@ def getUserID(email):
         return user.id
     except:
         return None
+
 
 @app.route('/')
 @app.route('/ItemCatalog')
@@ -175,7 +176,9 @@ def viewCategories():
     categories = session.query(Category).all()
     return render_template('categoryView.html', categories=categories)
 
-@app.route('/ItemCatalog/Categories/Add', methods=['GET','POST'])
+
+@app.route('/ItemCatalog/Categories/Add', methods=['GET', 'POST'])
+@login_required
 def addCategory():
     if request.method == 'GET':
         #return add category form
@@ -187,9 +190,11 @@ def addCategory():
         session.commit()
         return redirect(url_for('viewCategories'))
 
-@app.route('/ItemCatalog/Categories/<int:categoryID>/Edit', methods=['GET','POST'])
+
+@app.route('/ItemCatalog/Categories/<int:categoryID>/Edit', methods=['GET', 'POST'])
+@login_required
 def editCategory(categoryID):
-    
+
     category = session.query(Category).filter_by(id=categoryID).one()
     if request.method == 'GET':
         #return edit category form
@@ -201,7 +206,9 @@ def editCategory(categoryID):
         session.commit()
         return redirect(url_for('viewCategories'))
 
-@app.route('/ItemCatalog/Categories/<int:categoryID>/Delete', methods=['GET','POST'])
+
+@app.route('/ItemCatalog/Categories/<int:categoryID>/Delete', methods=['GET', 'POST'])
+@login_required
 def deleteCategory(categoryID):
     category = session.query(Category).filter_by(id=categoryID).one()
     if request.method == 'GET':
@@ -213,13 +220,16 @@ def deleteCategory(categoryID):
         session.commit()
         return redirect(url_for('viewCategories'))
 
+
 @app.route('/ItemCatalog/Categories/<int:categoryID>/Items/View')
 def viewItem(categoryID):
     category = session.query(Category).filter_by(id=categoryID).one()
     items = session.query(Item).filter_by(category_id=categoryID).all()
     return render_template('itemView.html', category=category, items=items)
 
-@app.route('/ItemCatalog/Categories/<int:categoryID>/Items/Add', methods=['GET','POST'])
+
+@app.route('/ItemCatalog/Categories/<int:categoryID>/Items/Add', methods=['GET', 'POST'])
+@login_required
 def addItem(categoryID):
     if request.method == 'GET':
         category = session.query(Category).filter_by(id=categoryID).one()
@@ -234,7 +244,9 @@ def addItem(categoryID):
         session.commit()
         return redirect(url_for('viewItem', categoryID=categoryID))
 
-@app.route('/ItemCatalog/Items/<int:itemID>/Edit', methods=['GET','POST'])
+
+@app.route('/ItemCatalog/Items/<int:itemID>/Edit', methods=['GET', 'POST'])
+@login_required
 def editItem(itemID):
     item = session.query(Item).filter_by(id=itemID).one()
     if request.method == 'GET':
@@ -248,7 +260,9 @@ def editItem(itemID):
         session.commit()
         return redirect(url_for('viewItem', categoryID=item.category_id))
 
-@app.route('/ItemCatalog/Items/<int:itemID>/Delete', methods=['GET','POST'])
+
+@app.route('/ItemCatalog/Items/<int:itemID>/Delete', methods=['GET', 'POST'])
+@login_required
 def deleteItem(itemID):
     item = session.query(Item).filter_by(id=itemID).one()
     if request.method == 'GET':
@@ -261,16 +275,17 @@ def deleteItem(itemID):
         session.commit()
         return redirect(url_for('viewItem', categoryID=category_id))
 
+
 @app.route('/ItemCatalog/api')
+@login_required
 def jsonifyAll():
     pass
     return 'json of all categories and items'
-    
+
+
 if(__name__ == '__main__'):
     app.config.update({
         'SECRET_KEY': 'asd098wer',
         'DEBUG': True,
     })
     app.run(host='0.0.0.0', port=8000)
-
-    
